f1268394ae333e1c42b5e109af5a7390ce6f5912
Denadi <denadihouenagnon@gmail.com>
2026-09-04 02:25:12 +0100

    Ajustar imagem

--- components/home/HeroNews.tsx
+++ components/home/HeroNews.tsx
@@ -1,12 +1,12 @@
 // components/home/HeroNews.tsx
 
 import { LatestNews } from "./LatestNews";
+import Image from "next/image";
 
 export function HeroNews() {
   return (
     <section className="border-b border-gray-300">
       <div className="mx-auto grid max-w-7xl grid-cols-1 gap-6 px-6 py-8 lg:grid-cols-[1fr_1.3fr_0.8fr]">
-
         {/* Texto */}
         <article>
           <h1 className="text-4xl font-bold leading-tight md:text-5xl">
@@ -14,23 +14,24 @@ export function HeroNews() {
           </h1>
 
           <p className="mt-6 text-lg leading-relaxed text-gray-600">
-            Esta é uma descrição da notícia principal com as
-            informações mais importantes.
+            Esta é uma descrição da notícia principal com as informações mais
+            importantes.
           </p>
         </article>
 
         {/* Imagem */}
         <div className="min-h-[350px] overflow-hidden">
-          <img
-            src="/news/public/"
-            alt="Notícia principal"
+          <Image
+            src="/president-donald-trump-9bvxk3h7h5b4uax5.jpg"
+            alt="Notícia em destaque"
+            width={800}
+            height={500}
             className="h-full w-full object-cover"
           />
         </div>
 
         {/* Últimas notícias */}
         <LatestNews />
-
       </div>
     </section>
   );
